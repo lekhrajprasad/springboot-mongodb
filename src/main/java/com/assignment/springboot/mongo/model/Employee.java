e4e1bea8ebb532ffc60558ef7a90aaf5ea058b42
lekhrajprasad <lekhraj.prasad@gmail.com>
2020-05-09 09:19:11 +0530

    updated to use lombok

--- src/main/java/com/assignment/springboot/mongo/model/Employee.java
+++ src/main/java/com/assignment/springboot/mongo/model/Employee.java
@@ -1,34 +1,46 @@
 package com.assignment.springboot.mongo.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 // Mongo database annotation.
-@Document(collection= "employee")
+@Document(collection = "employee")
+/*@Data
+@AllArgsConstructor
+@NoArgsConstructor*/
 public class Employee {
     @Id
     private int id;
     private String name;
     private String designation;
 
-    public Employee() { }
+    public Employee() {
+    }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDesignation() {
         return designation;
     }
+
     public void setDesignation(String designation) {
         this.designation = designation;
     }
